34c17f4b7746297f1fab667aae2372a6818e4b31
yopijay <judan.pauljohn@gmail.com>
2019-10-11 15:06:30 +0800

    Update ErrorProvider.java

--- app/src/main/java/com/example/ligers/ErrorProvider.java
+++ app/src/main/java/com/example/ligers/ErrorProvider.java
@@ -75,10 +75,12 @@ public class ErrorProvider  {
         return new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+
             }
 
             @Override
