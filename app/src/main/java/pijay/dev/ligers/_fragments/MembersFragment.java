1dbb6dc86c38db4fc705197603e6ace05a051a74
yopijay <judan.pauljohn@gmail.com>
2020-01-15 14:18:09 +0800

    Nothing, just adding the new icon for adding a members

--- app/src/main/java/pijay/dev/ligers/_fragments/MembersFragment.java
+++ app/src/main/java/pijay/dev/ligers/_fragments/MembersFragment.java
@@ -4,17 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.sdsmdg.tastytoast.TastyToast;
+
 import pijay.dev.ligers.R;
 import pijay.dev.ligers._class.CustomFieldDesign;
 import pijay.dev.ligers._class.CustomFont;
 
-public class MembersFragment extends Fragment {
+public class MembersFragment extends Fragment implements View.OnClickListener {
 
     //Layout
     View members;
@@ -23,6 +26,9 @@ public class MembersFragment extends Fragment {
     TextView member_title;
     TextView flyer_txt;
     TextView lifter_txt;
+
+    //EditTexts
+    EditText search_txtbox;
 
     @Nullable
     @Override
@@ -40,9 +46,20 @@ public class MembersFragment extends Fragment {
         flyer_txt = (TextView) CustomFieldDesign.design(flyer_txt, R.id.flyer_txt, 20, members);
         lifter_txt = (TextView) CustomFieldDesign.design(lifter_txt, R.id.lifter_txt, 20, members);
 
+        search_txtbox = (EditText) CustomFieldDesign.design(search_txtbox, R.id.search_txtbox, 15, members);
+
         CustomFont.Monoround.setFont(member_title, MembersFragment.this);
 
         CustomFont.Monoround.setFont(flyer_txt, MembersFragment.this);
         CustomFont.Monoround.setFont(lifter_txt, MembersFragment.this);
+
+        CustomFont.CenturyGothic.setFont(search_txtbox, MembersFragment.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+
+        }
     }
 }
